c74a0f94e48d434ecd04252b433c3af206634f59
frederik.steffen <frederik.steffen@gmail.com>
2018-12-29 22:32:12 +0100

    removed old seo component, removed filesystem plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,13 +24,6 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -41,8 +34,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        display: `standalone`,
       },
     },
     {
