1c7d2ac482cc7bff3f13711466c424b2cb838e08
frederik.steffen <frederik.steffen@gmail.com>
2018-12-31 18:48:45 +0100

    adjusted fab

--- src/templates/details.js
+++ src/templates/details.js
@@ -14,8 +14,8 @@ const styles = theme => ({
   },
   fab: {
     position: 'fixed',
-    bottom: theme.spacing.unit * 2,
-    right: theme.spacing.unit * 2,
+    bottom: theme.spacing.unit * 3,
+    right: theme.spacing.unit * 3,
   },
   hero: {
     marginTop: -theme.spacing.unit * 3,
