f7bb6298e83c421f44d5aee6f24d543071798c64
frederik.steffen <frederik.steffen@gmail.com>
2019-07-09 15:04:16 +0200

    fixed invisible cms images

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,20 +8,6 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          // gatsby-remark-relative-images must
-          // go before gatsby-remark-images
-          {
-            resolve: `gatsby-remark-relative-images`,
-          },
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 650,
-            },
-          },
           {
             resolve: `gatsby-remark-images-contentful`,
             options: {
